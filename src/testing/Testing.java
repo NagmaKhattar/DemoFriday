8922699f521a80c828bfefd421d1a9d8ec1e9918
NagmaKhattar <nagmakhattar@gmail.com>
2024-01-19 12:51:02 -0500

    initial commit- added author name and date

---
+++ src/testing/Testing.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testing;
+
+/**
+ *
+ * @author Nagma
+ * @modified Dec 19,2024
+ */
+public class Testing {
+
+    /**
+     * @param args
+     * the command
+     * line
+     * arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hi");   
+    }
+    
+}
